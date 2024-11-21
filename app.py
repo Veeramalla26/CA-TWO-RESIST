de1fa2f1793295146fd15e9ffaa0e9536b765c7d
Veeramalla26 <veeramallamanasagoud@gmail.com>
2024-11-21 15:42:47 +0000

    added user table

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html', message="This is Python assignment")
+# Configure PostgreSQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Manichandu123@localhost/hostelapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Run the app
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Define User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
